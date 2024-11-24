42742ab5acf8b7ab96781ff3d87971620218e0e7
hubinab <hubinab@gmail.com>
2024-11-24 17:51:19 +0100

    10-es lista

--- ikt/alkalmazott.py
+++ ikt/alkalmazott.py
@@ -5,6 +5,7 @@ t_alkalmazottak = file.load_json ("alkalmazottak.json")
 
 m_fomenu (list=t_alkalmazottak)
 
-kereses.lista(t_alkalmazottak)
+#kereses.lista(t_alkalmazottak)
+kereses.lista10(t_alkalmazottak)
 
 file.save_json ("alkalmazottak.json", t_alkalmazottak)
--- ikt/kereses.py
+++ ikt/kereses.py
@@ -1,4 +1,4 @@
-
+# Elsődleges kulcs szerinti keresés
 def pri_keres (nev, szev, list):
     search_nev = nev
     search_szev = szev
@@ -7,8 +7,40 @@ def pri_keres (nev, szev, list):
             return i
     return None
 
-def lista (list):
-    print(f"{"#":<3} {"Név":<40} {"Sz.év":>5} {"Fizetés":>10} {"Státusz":<7} {"St.év":>5} {"In.év":5}")
-    print("-"*81)
-    for i in range(len(list)):
-        print(f"{i+1:<3} {list[i]["NEV"]:<40} {list[i]["SZEV"]:>5} {list[i]['FIZU']:>10} {list[i]["STAT"]:<7} {list[i]["STARTEV"]:>5} {list[i]["INAKTEV"]:>5}")
+# Összes rekord listázása név szerint rendezve
+def lista (list, tol=None, ig=None):
+
+    if tol == None:
+        tol = 0
+    if ig == None:
+        ig = len(list)
+
+    # Rendezés
+    sort_list = sorted(list, key=lambda x: x["NEV"])
+
+    # Fejléc
+    print(f"{"#":<3} {"Név":<40} {"Sz.év":>5} {"Fizetés":>15} {"Státusz":<7} {"St.év":>5} {"In.év":5}")
+    print("-"*86)
+
+    # Elemek kiírása
+    for i in range(tol, ig):
+        fizu = f"{sort_list[i]['FIZU']:,}"
+        print(f"{i+1:<3} {sort_list[i]["NEV"]:<40} {sort_list[i]["SZEV"]:>5} {fizu:>15} {sort_list[i]["STAT"]:<7} {sort_list[i]["STARTEV"]:>5} {sort_list[i]["INAKTEV"]:>5}")
+
+# Összes rekord listázása 10-esével
+def lista10 (list):
+    inp = ""
+    i = 0
+    while inp != "#":
+        j = i+9
+        if j >= len(list):
+            j = len(list)
+
+        lista(list, i, j)
+        i = j
+        if i >= len(list):
+            break
+        
+        inp = input("Tovább=Enter, Kilépés=# -->")
+
+        
